459f5ac483d09b82762c5fe441ca3ff55cd85cd0
yujiuqie <yujiuqie@163.com>
2021-12-30 09:50:23 +0800

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_27_100236) do
-
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 2021_12_29_093310) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
